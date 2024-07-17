c862921f7dd9271d489fb390df11c9fa1ce1720c
leloiacj <166014920+leloiacj@users.noreply.github.com>
2024-07-17 13:03:57 +0200

    UpdateDatabaseFixed

---
+++ Migrations/20240717110227_MigrationbooksDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ser_PracticesProj.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationbooksDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
